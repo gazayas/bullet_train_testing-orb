80f62372976abec86a67f7b91f488f5eb6cc6ec1
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-05 22:14:26 +0900

    Add initial code for local minitest command

---
+++ src/scripts/local_minitest.sh
@@ -0,0 +1,7 @@
+if [[ ${knapsackPro} == true ]]; then
+  export RAILS_ENV=test
+  bundle exec rails "knapsack_pro:queue:minitest[--verbose]"
+else
+  bundle exec rails test
+  bundle exec rails test:system
+fi
