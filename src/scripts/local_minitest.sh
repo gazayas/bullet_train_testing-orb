d5971fb433f424954c5a549593dc5b7e316960c2
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-05 22:43:07 +0900

    Minor adjustments for local mintest command

--- src/scripts/local_minitest.sh
+++ src/scripts/local_minitest.sh
@@ -1,7 +1,9 @@
 if [[ $(("${KNAPSACK_PRO}")) == true ]]; then
+  echo "Running tests with Knapsack Pro."
   export RAILS_ENV=test
   bundle exec rails "knapsack_pro:queue:minitest[--verbose]"
 else
+  echo "Running tests without Knapsack Pro."
   bundle exec rails test
   bundle exec rails test:system
 fi
