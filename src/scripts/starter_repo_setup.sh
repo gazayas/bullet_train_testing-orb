2b7e00be054004708a4532b306803a33110c553f
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-06 20:55:51 +0900

    Edit starter repo setup bash script

--- src/scripts/starter_repo_setup.sh
+++ src/scripts/starter_repo_setup.sh
@@ -2,11 +2,21 @@
 # the strings are too long. They suggest writing bash files
 # and using the `include` syntax to run these files there.
 
+NAME=$(("${PACKAGE_NAME}"))
+
 # Clone the repository
 git clone https://github.com/bullet-train-co/bullet_train.git tmp/starter
 
 # Rename the directory of the Ruby gem being tested for the partial resolver
-mv /home/circleci/project /home/circleci/$(("${PACKAGE_NAME}")) && ln -s /home/circleci/$(("${PACKAGE_NAME}")) /home/circleci/project
+mv /home/circleci/project /home/circleci/$PACKAGE_NAME && ln -s /home/circleci/$PACKAGE_NAME /home/circleci/project
+
+# Use `bullet_Train` if the package is `bullet_train-base`
+if [[ $NAME =~ base$ ]]; then
+  NAME="bullet_train"
+fi
+
+GEM_STRING="gem \"${NAME}\""
+GEM_STRING_WITH_PATH="${GEM_STRING}, path: \"../..\""
 
 # Link starter repository to the Ruby gem being tested.
-grep -v 'gem \"bullet_train\"' tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && echo 'gem \"bullet_train\", path: \"../..\"' >> tmp/starter/Gemfile
+grep -v $GEM_STRING tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && echo 'gem \"bullet_train\", path: \"../..\"' >> tmp/starter/Gemfile
