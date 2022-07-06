b08a345c057eb3dd10804eab312157702ccdb485
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-06 21:02:32 +0900

    Edit starter repo setup bash script

--- src/scripts/starter_repo_setup.sh
+++ src/scripts/starter_repo_setup.sh
@@ -19,4 +19,4 @@ GEM_STRING=(-lh "gem \"${NAME}\"")
 GEM_STRING_WITH_PATH=(-lh "${GEM_STRING[@]}, path: \"../..\"")
 
 # Link starter repository to the Ruby gem being tested.
-grep -v ${GEM_STRING[@]} tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && echo ${GEM_STRING_WITH_PATH[@]} >> tmp/starter/Gemfile
+grep -v "${GEM_STRING[@]}" tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && echo "${GEM_STRING_WITH_PATH[@]}" >> tmp/starter/Gemfile
