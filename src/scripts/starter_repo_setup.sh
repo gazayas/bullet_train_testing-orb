d662f5cec0d88061268fe2823a7044d7e1af024c
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-06 20:44:05 +0900

    Add Bullet Train gem

--- src/scripts/starter_repo_setup.sh
+++ src/scripts/starter_repo_setup.sh
@@ -6,10 +6,7 @@
 git clone https://github.com/bullet-train-co/bullet_train.git tmp/starter
 
 # Rename the directory of the Ruby gem being tested for the partial resolver
-mv /home/circleci/project /home/circleci/$(("${PACKAGE_NAME}")) && \
-ln -s /home/circleci/$(("${PACKAGE_NAME}")) /home/circleci/project
+mv /home/circleci/project /home/circleci/$(("${PACKAGE_NAME}")) && ln -s /home/circleci/$(("${PACKAGE_NAME}")) /home/circleci/project
 
 # Link starter repository to the Ruby gem being tested.
-grep -v 'gem \"bullet_train\"' tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && \
-mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && \
-echo 'gem \"bullet_train\", path: \"../..\"' >> tmp/starter/Gemfile
+grep -v 'gem \"bullet_train\"' tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && echo 'gem \"bullet_train\", path: \"../..\"' >> tmp/starter/Gemfile
