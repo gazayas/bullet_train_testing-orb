544a5ebbdd280995be0e9e0c1eda6f2cf4f7c99e
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-06 20:38:29 +0900

    Extract long bash commands to bash file

---
+++ src/scripts/starter_repo_setup.sh
@@ -0,0 +1,15 @@
+# CircleCI won't let us put these in the yml file since
+# the strings are too long. They suggest writing bash files
+# and using the `include` syntax to run these files there.
+
+# Clone the repository
+git clone https://github.com/bullet-train-co/bullet_train.git tmp/starter
+
+# Rename the directory of the Ruby gem being tested for the partial resolver
+mv /home/circleci/project /home/circleci/$(("${PACKAGE_NAME})) && \
+ln -s /home/circleci/$(("${PACKAGE_NAME})) /home/circleci/project
+
+# Link starter repository to the Ruby gem being tested.
+grep -v 'gem \"bullet_train\"' tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && \
+mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && \
+echo 'gem \"bullet_train\", path: \"../..\"' >> tmp/starter/Gemfile
