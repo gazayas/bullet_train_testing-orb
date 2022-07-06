4a5b7a55b0221d9e57ebfa85f5f17092b5bbd03d
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-06 20:39:15 +0900

    Fix shell error

--- src/scripts/starter_repo_setup.sh
+++ src/scripts/starter_repo_setup.sh
@@ -6,8 +6,8 @@
 git clone https://github.com/bullet-train-co/bullet_train.git tmp/starter
 
 # Rename the directory of the Ruby gem being tested for the partial resolver
-mv /home/circleci/project /home/circleci/$(("${PACKAGE_NAME})) && \
-ln -s /home/circleci/$(("${PACKAGE_NAME})) /home/circleci/project
+mv /home/circleci/project /home/circleci/$(("${PACKAGE_NAME}")) && \
+ln -s /home/circleci/$(("${PACKAGE_NAME}")) /home/circleci/project
 
 # Link starter repository to the Ruby gem being tested.
 grep -v 'gem \"bullet_train\"' tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && \
