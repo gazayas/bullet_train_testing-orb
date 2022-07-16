5404fe6f57906e077c0a4d429664f9b268d57bdf
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-16 09:28:47 +0900

    Isolate link gem logic to its own script

--- src/scripts/starter_repo_setup.sh
+++ src/scripts/starter_repo_setup.sh
@@ -16,11 +16,3 @@ ln -s /home/circleci/$PACKAGE_NAME /home/circleci/project
 if [[ $NAME =~ base$ ]]; then
   NAME="bullet_train"
 fi
-
-GEM_STRING=("gem \"${NAME}\"")
-GEM_STRING_WITH_PATH=("${GEM_STRING[@]}, path: \"../..\"")
-
-# Link starter repository to the Ruby gem being tested.
-grep -v "${GEM_STRING[@]}" tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && \
-mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && \
-echo "${GEM_STRING_WITH_PATH[@]}" >> tmp/starter/Gemfile
