2672d0aee5e8beecd0609399599fa8ff662028c9
Gabriel Zayas <g-zayas@hotmail.com>
2022-08-01 18:48:43 +0900

    Run link_gem commands individually

--- src/scripts/link_gem.sh
+++ src/scripts/link_gem.sh
@@ -4,6 +4,6 @@ GEM_STRING=("gem \"${NAME}\"")
 GEM_STRING_WITH_PATH=("${GEM_STRING[@]}, path: \"../..\"")
 
 # Link starter repository to the Ruby gem being tested.
-grep -v "${GEM_STRING[@]}" tmp/starter/Gemfile > tmp/starter/Gemfile.tmp && \
-mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile && \
+grep -v "${GEM_STRING[@]}" tmp/starter/Gemfile > tmp/starter/Gemfile.tmp
+mv tmp/starter/Gemfile.tmp tmp/starter/Gemfile
 echo "${GEM_STRING_WITH_PATH[@]}" >> tmp/starter/Gemfile
